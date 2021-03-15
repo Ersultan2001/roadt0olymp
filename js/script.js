758a166505136b64cda2f7db91a2ab576aea35b4
Ersultan <example@gmail.com>
2021-03-15 16:17:06 +0600

    script file

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
 "use strict";
 
 
+let number0fFilms=+prompt('Сколько фильмов вы уже посмотрели?','');
+
+
+const personalMovieDB = {
+    count: number0fFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних посмотренных фильмов?',''),
+    b = prompt('На сколкьо оцените его?',''),
+    c = prompt('Один из последних посмотренных фильмов?',''),
+    d = prompt('На сколкьо оцените его?','');
+
+
+    
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
