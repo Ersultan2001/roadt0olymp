6e1558eb1333d5d0b8d25a2a89dace51726eb314
Ersultan <example@gmail.com>
2021-03-15 16:31:28 +0600

    if js

--- js/if.js
+++ js/if.js
@@ -6,3 +6,32 @@ if (4){
 }else{
     console.log('error');
 }
+
+// if(num < 49){
+//     console.log('error');
+// }else if (num > 100){
+//     console.log('Много');
+// }else{
+//     console.log('ОК');
+// }
+
+// (num === 50) ?   console.log('ОК') : console.log('error');
+
+
+const num = '50';
+
+switch(num){ 
+    case '49':
+        console.log('Мало');
+    break;
+    case '100':
+        console.log('Много');
+    break;
+    case '50':
+    console.log('Верно');
+    break;
+
+    default:
+        console.log('Не в этот раз');
+        break;
+}
