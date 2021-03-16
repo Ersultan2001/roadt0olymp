3fb032eb2d722fb43b6c2e2661e684e39d8a350f
Ersultan <example@gmail.com>
2021-03-16 11:38:55 +0600

    pff

--- js/if.js
+++ js/if.js
@@ -1,37 +1,37 @@
-"use strict";
+// "use strict";
 
 
-if (4){
-    console.log('Ok');
-}else{
-    console.log('error');
-}
-
-// if(num < 49){
-//     console.log('error');
-// }else if (num > 100){
-//     console.log('Много');
+// if (4){
+//     console.log('Ok');
 // }else{
-//     console.log('ОК');
+//     console.log('error');
 // }
 
-// (num === 50) ?   console.log('ОК') : console.log('error');
+// // if(num < 49){
+// //     console.log('error');
+// // }else if (num > 100){
+// //     console.log('Много');
+// // }else{
+// //     console.log('ОК');
+// // }
+
+// // (num === 50) ?   console.log('ОК') : console.log('error');
 
 
-const num = '50';
+// const num = '50';
 
-switch(num){ 
-    case '49':
-        console.log('Мало');
-    break;
-    case '100':
-        console.log('Много');
-    break;
-    case '50':
-    console.log('Верно');
-    break;
+// switch(num){ 
+//     case '49':
+//         console.log('Мало');
+//     break;
+//     case '100':
+//         console.log('Много');
+//     break;
+//     case '50':
+//     console.log('Верно');
+//     break;
 
-    default:
-        console.log('Не в этот раз');
-        break;
-}
+//     default:
+//         console.log('Не в этот раз');
+//         break;
+// }
